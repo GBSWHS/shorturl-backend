3bf22fcacc592e4c6b6cfd1a981ca3daa7c45c9c
Park Min Hyeok <opensource@pmh.codes>
2022-05-09 15:39:37 +0900

    feat: add url regist api

--- shorturl-backend/app.ts
+++ shorturl-backend/app.ts
@@ -1,11 +1,18 @@
-import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { CloudFrontResponse, CloudFrontResponseHandler } from 'aws-lambda';
 
+const IP_ALLOWLIST = process.env.IP_ALLOWLIST;
 const HOME_PAGE_URL = 'http://school.gyo6.net/gbsw';
 const DB_TABLE_NAME = 'urlshorter-db';
 const DB_REGION_NAME = 'ap-northeast-2';
 
 const dynamoClient = new DynamoDBClient({ region: DB_REGION_NAME });
+
+const sendCreated = (): CloudFrontResponse => ({
+    status: '201',
+    statusDescription: 'Created',
+    headers: {},
+});
 
 const redirect = (url: string): CloudFrontResponse => ({
     status: '302',
@@ -18,6 +25,30 @@ const redirect = (url: string): CloudFrontResponse => ({
             },
         ],
     },
+});
+
+const sendConflictError = (): CloudFrontResponse => ({
+    status: '409',
+    statusDescription: 'Conflict',
+    headers: {},
+});
+
+const sendUnauthorizedError = (): CloudFrontResponse => ({
+    status: '401',
+    statusDescription: 'Unauthorized',
+    headers: {},
+});
+
+const sendBadRequestError = (): CloudFrontResponse => ({
+    status: '400',
+    statusDescription: 'Bad Request',
+    headers: {},
+});
+
+const sendServerError = (): CloudFrontResponse => ({
+    status: '500',
+    statusDescription: 'Service Unavailable',
+    headers: {},
 });
 
 const getLongURL = async (shortPath: string): Promise<string | undefined> => {
@@ -39,10 +70,56 @@ const getLongURL = async (shortPath: string): Promise<string | undefined> => {
     }
 };
 
+const putRecord = async (shortPath: string, longURL: string) => {
+    const command = new PutItemCommand({
+        TableName: DB_TABLE_NAME,
+        Item: {
+            short_path: {
+                S: shortPath,
+            },
+            long_url: {
+                S: longURL,
+            },
+        },
+    });
+
+    try {
+        await dynamoClient.send(command);
+        return true;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+};
+
 const lambdaFunction: CloudFrontResponseHandler = async (event) => {
     const requestURI = event.Records[0].cf.request.uri;
     if (requestURI === '/') {
         return redirect(HOME_PAGE_URL);
+    }
+
+    if (requestURI === '/api/regist') {
+        const q = new URLSearchParams(event.Records[0].cf.request.querystring);
+
+        if (event.Records[0].cf.request.clientIp !== IP_ALLOWLIST) {
+            return sendUnauthorizedError();
+        }
+
+        const s = q.get('s');
+        const l = q.get('l');
+
+        if (!s || !l) return sendBadRequestError();
+        if (s.length < 4 || s.length > 20) return sendBadRequestError();
+        if (l.length < 4 || l.length > 255) return sendBadRequestError();
+
+        if (await getLongURL(q.get('s') || '')) {
+            return sendConflictError();
+        }
+
+        const success = putRecord(s, l);
+
+        if (!success) return sendServerError();
+        return sendCreated();
     }
 
     const longURL = await getLongURL(requestURI);
